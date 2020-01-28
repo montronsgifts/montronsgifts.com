981f1e560693581fc4395e4ddfda112dcbff78aa
Monica Buettner <monica@Monicas-MacBook-Pro.local>
2020-01-27 21:01:23 -0500

    Reduced file size of blanket images

--- src/components/ApplicationBar.vue
+++ src/components/ApplicationBar.vue
@@ -1,20 +1,58 @@
 <template>
-  <v-navigation-drawer
-    app
-    permanent
-    color="indigo"
-    dark
-  >
-    <v-list
-      dense
-      nav
-      class="py-0"
+  <v-nav>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      color="indigo"
+      dark
     >
-      <v-list-item to="/" > Home/About </v-list-item>
-      <v-list-item to="/blankets"> Blankets </v-list-item>
-      <v-list-item to="/scarves"> Scarves </v-list-item>
-      <v-list-item to="/books"> Books </v-list-item>
-      <v-list-item to="/contactme"> Contact Me </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
+      <v-list dense>
+        <v-list-item nav to="/">
+          <v-list-item-content>
+            <v-list-item-title>Home/About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item nav to="/blankets">
+          <v-list-item-content>
+            <v-list-item-title>Blankets</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item nav to="/scarves">
+          <v-list-item-content>
+            <v-list-item-title>Scarves</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item nav to="/books">
+          <v-list-item-content>
+            <v-list-item-title>Books</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item nav to="/contactme">
+          <v-list-item-content>
+            <v-list-item-title>Contact Me</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      permanent
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
+  </v-nav>
 </template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+  }
+</script>
--- src/components/Blankets.vue
+++ src/components/Blankets.vue
@@ -42,122 +42,122 @@
           {
               name: "Yellow & Gray Elephant with Gray Backing",
               type: "tie",
-              image: require("../assets/blankets/elephant-tie-blanket.jpeg")
+              image: require("../assets/blankets/elephant-tie-blanket.jpg")
           },
           {
               name: "Blue, Orange & White Chunky Stitch",
               type: "crochet",
-              image: require("../assets/blankets/blue-orange-blanket.jpeg")
+              image: require("../assets/blankets/blue-orange-blanket.jpg")
           },
           {
               name: "Green, Orange, Blue & White Granny Stripe Stitch",
               type: "crochet",
-              image: require("../assets/blankets/green-orange-blue-blanket.jpeg")
+              image: require("../assets/blankets/green-orange-blue-blanket.jpg")
           },
           {
               name: "Purple, Pink & White Half-Double Stitch",
               type: "crochet",
-              image: require("../assets/blankets/purple-blanket.jpeg")
+              image: require("../assets/blankets/purple-blanket.jpg")
           },
           {
               name: "Purple, Lavendar & Gray Chunky Stitch",
               type: "crochet",
-              image: require("../assets/blankets/thick-purple-blanket.jpeg")
+              image: require("../assets/blankets/thick-purple-blanket.jpg")
           },
           {
               name: "Turquoise, White, Coral, Light Green & Gray Shell Stitch",
               type: "crochet",
-              image: require("../assets/blankets/shell-blanket.jpeg")
+              image: require("../assets/blankets/shell-blanket.jpg")
           },
           {
               name: "Yellow, Gray & White Chunky Stitch",
               type: "crochet",
-              image: require("../assets/blankets/yellow-gray-blanket.jpeg")
+              image: require("../assets/blankets/yellow-gray-blanket.jpg")
           },
           {
               name: "Baseball with Red Backing",
               type: "tie",
-              image: require("../assets/blankets/baseball-tie-blanket.jpeg")
+              image: require("../assets/blankets/baseball-tie-blanket.jpg")
           },
           {
               name: "Bear & Moose with Plaid Backing",
               type: "tie",
-              image: require("../assets/blankets/bear-tie-blanket.jpeg")
+              image: require("../assets/blankets/bear-tie-blanket.jpg")
           },
           {
               name: "Black, White & Gray Designed with Black Designed Backing",
               type: "tie",
-              image: require("../assets/blankets/black-tie-blanket.jpeg")
+              image: require("../assets/blankets/black-tie-blanket.jpg")
           },
           {
               name: "Dinosaurs with Orange Backing",
               type: "tie",
-              image: require("../assets/blankets/orange-dino-tie-blanket.jpeg")
+              image: require("../assets/blankets/orange-dino-tie-blanket.jpg")
           },
           {
               name: "Dinosaurs with Lime Green Backing",
               type: "tie",
-              image: require("../assets/blankets/green-dino-tie-blanket.jpeg")
+              image: require("../assets/blankets/green-dino-tie-blanket.jpg")
           },
           {
               name: "Zoo Animals with Pink Backing",
               type: "tie",
-              image: require("../assets/blankets/animal-tie-blanket.jpeg")
+              image: require("../assets/blankets/animal-tie-blanket.jpg")
           },
           {
               name: "Marvel with Green Backing",
               type: "tie",
-              image: require("../assets/blankets/marvel-tie-blanket.jpeg")
+              image: require("../assets/blankets/marvel-tie-blanket.jpg")
           },
           {
               name: "Owls with Brown Backing",
               type: "tie",
-              image: require("../assets/blankets/owl-tie-blanket.jpeg")
+              image: require("../assets/blankets/owl-tie-blanket.jpg")
           },
           {
               name: "Monkeys with Brown Backing",
               type: "tie",
-              image: require("../assets/blankets/monkey-tie-blanket.jpeg")
+              image: require("../assets/blankets/monkey-tie-blanket.jpg")
           },
           {
               name: "Forest Animals with Green Paw Print Backing",
               type: "tie",
-              image: require("../assets/blankets/forrest-tie-blanket.jpeg")
+              image: require("../assets/blankets/forrest-tie-blanket.jpg")
           },
           {
               name: "Puzzle with Pink Backing",
               type: "tie",
-              image: require("../assets/blankets/puzzle-tie-blanket.jpeg")
+              image: require("../assets/blankets/puzzle-tie-blanket.jpg")
           },
           {
               name: "Dumptrucks with Red Backing",
               type: "tie",
-              image: require("../assets/blankets/dumptruck-tie-blanket.jpeg")
+              image: require("../assets/blankets/dumptruck-tie-blanket.jpg")
           },
           {
               name: "Flamingos with Pink Backing",
               type: "tie",
-              image: require("../assets/blankets/flamingo-tie-blanket.jpeg")
+              image: require("../assets/blankets/flamingo-tie-blanket.jpg")
           },
           {
               name: "Elephants with Gray Backing",
               type: "tie",
-              image: require("../assets/blankets/blue-elephant-tie-blanket.jpeg")
+              image: require("../assets/blankets/blue-elephant-tie-blanket.jpg")
           },
           {
               name: "Purple & Pink Plaid with Pink Backing",
               type: "tie",
-              image: require("../assets/blankets/pink-plaid-tie-blanket.jpeg")
+              image: require("../assets/blankets/pink-plaid-tie-blanket.jpg")
           },
           {
               name: "Elephant Sweet Baby with Purple Backing",
               type: "tie",
-              image: require("../assets/blankets/sweet-elephant.jpeg")
+              image: require("../assets/blankets/sweet-elephant.jpg")
           },
           {
               name: "Dinosaur T-rex with Gray Stone Backing",
               type: "tie",
-              image: require("../assets/blankets/dino-trex.jpeg")
+              image: require("../assets/blankets/dino-trex.jpg")
           }
 
         ]
